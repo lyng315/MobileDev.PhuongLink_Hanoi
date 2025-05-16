de4d553c741ee3c971a8dfbd401fbe1d953b1ebf
TranQuocTroq <tranquoctroq@gmail.com>
2025-05-16 23:44:08 +0700

    delete

--- Admin_PhuongLink_Hanoi/Admin_PhuongLink_Hanoi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Admin_PhuongLink_Hanoi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
