c604bcfcf11b0459493b5cac6b38fe2b67824897
lyng315 <2251061820@e.tlu.edu.vn>
2025-05-18 23:09:52 +0700

    Add Category Management

---
+++ Admin/WebApplication1/WebApplication1/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        // GET: /Admin/Dashboard
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Admin/WebApplication1/WebApplication1/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplication1.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        // GET: /Admin/User
+        public IActionResult Index()
+        {
+            // TODO: gọi service lấy danh sách users
+            // var users = _userService.GetAll();
+            // return View(users);
+
+            return View(); // tạm thời
+        }
+    }
+}
