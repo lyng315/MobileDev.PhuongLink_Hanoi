9cdc591d690da29adbc4c45c6509915e29641415
Lovlam-516 <lam6a2nnbk@gmail.com>
2025-06-12 17:09:35 +0700

    update Web

---
+++ web/WebApplication1/Models/ViewModels/CommunityDashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models.ViewModels
+{
+    public class CommunityDashboardViewModel
+    {
+        public int TotalPosts { get; set; }
+        public int TotalComments { get; set; }
+        public int TotalUsers { get; set; }
+
+        public int GrowthPostPercent { get; set; }
+        public int GrowthCommentPercent { get; set; }
+        public int GrowthUserPercent { get; set; }
+    }
+}
